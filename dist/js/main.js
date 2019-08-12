133451024487f9aa778f516f1c38462310f39da4
Alan Dominiczak <alan.dominiczak10@gmail.com>
2019-08-12 17:28:04 +0200

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,153 +1,114 @@
-// var mySwiper = new Swiper('.swiper-container-h', {
-// 	// Optional parameters
-// 	direction: 'vertical',
-// 	loop: false,
-// 	speed: 1000,
-// 	mousewheel: true,
-// 	keyboard: {
-// 		enabled: true,
-// 	},
-// 	pagination: {
-// 		el: '.swiper-pagination-h',
-// 		renderBullet: function (index, className) {
-// 			var section = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-section');
-// 			return '<span class="' + className + '">' + section + '</span>';
-// 		},
-// 		clickable: true
-// 	},
-// 	navigation: {
-// 		nextEl: '.swiper-button-next',
-// 		prevEl: '.swiper-button-prev',
-// 	},
-// })
+const init = (() => {
+	/* ------------------------------------------------------------------------------------------------------------------------------------------------ */
+	/* ------------------------------------------------------------------------------------------------------------------------------------------------ */
+	// Initialize Swiper
+	const breakpoint = window.matchMedia('(min-width:56.25em)');
+	// Keep track of swiper instances to destroy later
+	let mySwiper;
+	const breakpointChecker = function () {
+		if (breakpoint.matches === true) {
+			return enableSwiper();
+		} else if (breakpoint.matches === false) {
+			if (mySwiper !== undefined) mySwiper.destroy(true, true); // Destroy swiper on mobile
+			return;
+		}
+	};
 
-// breakpoint where swiper will be destroyed
-// and switches to a dual-column layout
-const breakpoint = window.matchMedia('(min-width:56.25em)');
-// keep track of swiper instances to destroy later
-let mySwiper;
-//////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////
-const breakpointChecker = function () {
-	if (breakpoint.matches === true) {
-
-		return enableSwiper();
-
-	} else if (breakpoint.matches === false) {
-		if (mySwiper !== undefined) mySwiper.destroy(true, true);
-		// document.querySelectorAll('section').forEach(section => section.classList.remove('swiper-slide'));
-		// document.querySelector('.swiper-wrapper').classList.remove('swiper-wrapper');
-		// document.querySelector('.swiper-container-h').classList.remove('swiper-container-h');
-		// or/and do nothing
-		return;
-	}
-};
-//////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////
-const enableSwiper = function () {
-	mySwiper = new Swiper('.swiper-container', {
-		// Optional parameters
-		direction: 'vertical',
-		loop: false,
-		speed: 1000,
-		mousewheel: true,
-		keyboard: {
-			enabled: true,
-		},
-		pagination: {
-			el: '.swiper-pagination-h',
-			renderBullet: function (index, className) {
-				var section = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-section');
-				return '<span class="' + className + '">' + section + '</span>';
+	const enableSwiper = function () {
+		mySwiper = new Swiper('.swiper-container', {
+			// Optional parameters
+			direction: 'vertical',
+			loop: false,
+			speed: 1000,
+			mousewheel: true,
+			keyboard: {
+				enabled: true,
 			},
-			clickable: true
-		},
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-	})
+			pagination: {
+				el: '.swiper-pagination-h',
+				renderBullet: function (index, className) {
+					var section = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-section');
+					return '<span class="' + className + '">' + section + '</span>';
+				},
+				clickable: true
+			},
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+		})
 
-	document.querySelector('.hero__btn').addEventListener('click', (e) => {
-		e.preventDefault();
-		mySwiper.slideTo(5, 1500);
-	})
-};
+		// Add swiper to cta button -> scroll to contact section
+		document.querySelector('.hero__btn').addEventListener('click', (e) => {
+			e.preventDefault();
+			mySwiper.slideTo(5, 1500); // 5 - contact section
+		})
+	};
 
+	// Keep an eye on viewport size changes
+	breakpoint.addListener(breakpointChecker);
+	// Kickstart
+	breakpointChecker();
 
-//////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////
-// keep an eye on viewport size changes
-breakpoint.addListener(breakpointChecker);
-// kickstart
-breakpointChecker();
-
-// PROJECTS SLIDER
-const flkty = new Flickity('.projects-gallery', {
-	// options
-	cellAlign: 'left',
-	contain: true,
-});
-
-// HAMBURGER MENU
-const menuIcon = document.querySelector('.header__hamburger');
-const nav = document.querySelector('.navigation');
-const navLinks = [...document.querySelectorAll('.navigation__link')];
-console.log(navLinks);
-
-menuIcon.addEventListener('click', () => {
-	menuIcon.classList.toggle('change');
-	nav.classList.toggle('change');
-});
-
-navLinks.forEach(link => {
-	link.addEventListener('click', () => {
-		nav.classList.remove('change');
-		menuIcon.classList.remove('change');
+	/* ------------------------------------------------------------------------------------------------------------------------------------------------ */
+	/* ------------------------------------------------------------------------------------------------------------------------------------------------ */
+	// INITIALIZE DRAGGABLE PROJECTS SLIDER
+	const flkty = new Flickity('.projects-gallery', {
+		// options
+		cellAlign: 'left',
+		contain: true,
 	});
-});
 
-// INITIALIZE SMOOTH SCROLL
-const scroll = new SmoothScroll('.navigation a[href*="#"]', {
-	speed: 700
-});
+	/* ------------------------------------------------------------------------------------------------------------------------------------------------ */
+	/* ------------------------------------------------------------------------------------------------------------------------------------------------ */
+	// HAMBURGER MENU
+	const menuIcon = document.querySelector('.header__hamburger');
+	const nav = document.querySelector('.navigation');
+	const navLinks = [...document.querySelectorAll('.navigation__link')];
 
-const cta = new SmoothScroll('.hero__caption-wrapper a[href*="#"]', {
-	speed: 700
-});
+	menuIcon.addEventListener('click', () => {
+		menuIcon.classList.toggle('change');
+		nav.classList.toggle('change');
+	});
 
-
-
-// INITIALIZE ANIMATE ON SCROLL ON WIDTH <= 900px
-// const width = window.innerWidth;
-let scrollAnimations;
-
-// if ((window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) <= 900) {
-// 	scrollAnimations = sal({
-// 		disabled: false,
-// 		once: false,
-// 		treshold: 1
-// 	});
-// } else {
-// 	scrollAnimations = sal({
-// 		disabled: true
-// 	});
-// }
-
-scrollAnimations = sal({
-	once: false
-});
+	navLinks.forEach(link => {
+		link.addEventListener('click', () => {
+			nav.classList.remove('change');
+			menuIcon.classList.remove('change');
+		});
+	});
 
 
+	/* ------------------------------------------------------------------------------------------------------------------------------------------------ */
+	/* ------------------------------------------------------------------------------------------------------------------------------------------------ */
+	// INITIALIZE SMOOTH SCROLL 
+	// -> menu items
+	const scroll = new SmoothScroll('.navigation a[href*="#"]', {
+		speed: 700
+	});
+	// -> cta button
+	const cta = new SmoothScroll('.hero__caption-wrapper a[href*="#"]', {
+		speed: 700
+	});
 
-// PRELOADER
-window.addEventListener('load', () => {
-	const preloader = document.querySelector('.loader');
-	setTimeout(() => {
-		preloader.classList.add('loader-finish');
-	}, 2000);
-});
+	/* ------------------------------------------------------------------------------------------------------------------------------------------------ */
+	/* ------------------------------------------------------------------------------------------------------------------------------------------------ */
+	// INITIALIZE ANIMATE ON SCROLL
+	let scrollAnimations;
+
+	scrollAnimations = sal({
+		once: false
+	});
+
+	/* ------------------------------------------------------------------------------------------------------------------------------------------------ */
+	/* ------------------------------------------------------------------------------------------------------------------------------------------------ */
+	// PRELOADER - OPTIONS
+	window.addEventListener('load', () => {
+		const preloader = document.querySelector('.loader');
+		setTimeout(() => {
+			preloader.classList.add('loader-finish');
+		}, 2000);
+	});
+})();
+
 
